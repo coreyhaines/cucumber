3a1e3e5ea74b26e3a10bde110bcf03336e7fbad9
Leon Gersing & Corey Haines <coreyhaines+leon@gmail.com>
2009-12-22 15:23:21 -0500

    initial multi-module support

--- lib/cucumber/rb_support/rb_step_extensions.rb
+++ lib/cucumber/rb_support/rb_step_extensions.rb
@@ -1,33 +1,32 @@
 module Cucumber
   class StepExtensions
-    @@extension_modules = {}
 
-    def initialize(extension_args)
-      @extension_args = extension_args
+    def initialize(extension_args = [])
+      @extension_args = [extension_args].flatten
     end
+    require 'g'
 
-    def handle_arguments(args)
-      return args if @@extension_modules.empty? || @extension_args.values.empty?
-      
+    def handle_arguments(args = [])
+      return args if @extension_args.empty?
       run_extensions_on(args)
     end
 
     def run_extensions_on(args)
       [methods_for(args), args].transpose.map do |extension_method, arg|
-        @@extension_modules.values[0].send(extension_method, arg)
+        self.send(extension_method, arg)
       end
     end
 
     def methods_for(args)
-      methods = [@extension_args.values[0]].flatten
+      methods = @extension_args
       if(methods.size == 1)
         methods = (methods * args.size)
       end
       methods      
     end
 
-    def self.register_extension_module(extension_modules)
-      @@extension_modules.merge!(extension_modules)
+    def self.register_extension_module(*extension_modules)
+      extension_modules.each {|ext| self.send(:include,ext) }
     end
   end
 end
--- spec/cucumber/rb_support/rb_support_step_extensions_spec.rb
+++ spec/cucumber/rb_support/rb_support_step_extensions_spec.rb
@@ -4,7 +4,7 @@ require 'cucumber/rb_support/rb_step_extensions'
 describe Cucumber::StepExtensions do
   context "no extensions registered" do
     it "returns the passed in args" do
-      ext = Cucumber::StepExtensions.new({})
+      ext = Cucumber::StepExtensions.new
       ext.handle_arguments([:corey]).should == [:corey]
     end
   end
@@ -21,10 +21,10 @@ describe Cucumber::StepExtensions do
         end
       end
 
-      Cucumber::StepExtensions.register_extension_module(:reverser_extension => MyStepExtensions)
+      Cucumber::StepExtensions.register_extension_module(MyStepExtensions)
     end
     it "returns the passed in args" do
-      ext = Cucumber::StepExtensions.new({})
+      ext = Cucumber::StepExtensions.new
       ext.handle_arguments([:corey]).should == [:corey]
     end
   end
@@ -32,35 +32,35 @@ describe Cucumber::StepExtensions do
   context "one registered extensions module" do
     before(:each) do
       module MyStepExtensions
-        def self.reverse(arg)
+        def reverse(arg)
           arg.reverse
         end
 
-        def self.upcase(arg)
+        def upcase(arg)
           arg.upcase
         end
       end
 
-      Cucumber::StepExtensions.register_extension_module(:reverser_extension => MyStepExtensions)
+      Cucumber::StepExtensions.register_extension_module(MyStepExtensions)
     end
 
     it "uses the registered extension" do
-      extensions = Cucumber::StepExtensions.new({:reverser_extension => :reverse})
+      extensions = Cucumber::StepExtensions.new(:reverse)
       extensions.handle_arguments(['hello']).should == ['olleh']
     end
 
     it "applies a single request extension method to all arguments" do
-      se = Cucumber::StepExtensions.new({:reverser_extension => :reverse})
+      se = Cucumber::StepExtensions.new(:reverse)
       se.handle_arguments(["hello", "Leon"]).should == ["olleh", "noeL"] 
     end
 
     it "handles different extension methods" do
-      se = Cucumber::StepExtensions.new({:reverser_extension => :upcase})
+      se = Cucumber::StepExtensions.new(:upcase)
       se.handle_arguments(["hello"]).should == ["HELLO"] 
     end
 
     it "applies desired extension method to the appropriate argument" do
-      se = Cucumber::StepExtensions.new({:reverser_extension => [:reverse, :upcase]})
+      se = Cucumber::StepExtensions.new([:reverse, :upcase])
       se.handle_arguments(["hello", "Leon"]).should == ["olleh", "LEON"]
     end
   end
@@ -79,13 +79,18 @@ describe Cucumber::StepExtensions do
         end
       end
 
-      Cucumber::StepExtensions.register_extension_module(ReverseStepExtensions)
-      Cucumber::StepExtensions.register_extension_module(CapitalStepExtensions)
+      Cucumber::StepExtensions.register_extension_module(ReverseStepExtensions,
+                                                         CapitalStepExtensions)
     end
 
-    xit "handles specific calls to specific methods loaded" do
-      Cucumber::StepExtensions.new.handle_arguments(['hello']).should == ['olleh']
+    it "handles specific calls to specific methods loaded" do
+      extensions = Cucumber::StepExtensions.new(:reverse)
+      extensions.handle_arguments(['hello']).should == ['olleh']
     end
 
+    it 'handles calling different extension modules' do
+      extensions = Cucumber::StepExtensions.new([:reverse, :upcase])
+      extensions.handle_arguments(['hello', 'make_me_upper']).should == ['olleh', 'MAKE_ME_UPPER']
+    end
   end
 end
