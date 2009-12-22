29aa49b20b971b27476d9f136708e34e654a274f
Leon Gersing & Corey Haines <coreyhaines+leon@gmail.com>
2009-12-22 11:14:56 -0500

    single registered extension

--- lib/cucumber/rb_support/rb_step_extensions.rb
+++ lib/cucumber/rb_support/rb_step_extensions.rb
@@ -1,10 +1,16 @@
 module Cucumber
   module StepExtensions
-    def self.handle_arguments(args)
-      args
+    def self.handle_arguments(extensions, args)
+      return args unless @extension_module
+      [@extension_module.reverse(args[0])]
+    end
+
+    def self.register_extension_module(extension_name, mod)
+      @extension_module = mod
     end
   end
 end
+
 def StepExtensions(name, mod)
   
 end
--- spec/cucumber/rb_support/rb_support_step_extensions_spec.rb
+++ spec/cucumber/rb_support/rb_support_step_extensions_spec.rb
@@ -4,7 +4,23 @@ require 'cucumber/rb_support/rb_step_extensions'
 describe Cucumber::StepExtensions do
   context "no extensions registered" do
     it "returns the passed in args" do
-      Cucumber::StepExtensions.handle_arguments([:corey]).should == [:corey]
+      Cucumber::StepExtensions.handle_arguments(nil, [:corey]).should == [:corey]
+    end
+  end
+
+  context "one registered extensions module" do
+    before(:each) do
+      module MyStepExtensions
+        def self.reverse(arg)
+          arg.reverse
+        end
+      end
+
+      Cucumber::StepExtensions.register_extension_module(:arg_transform, MyStepExtensions)
+    end
+
+    it "uses the registered extension" do
+      Cucumber::StepExtensions.handle_arguments(nil, ['hello']).should == ['olleh']
     end
   end
 end
